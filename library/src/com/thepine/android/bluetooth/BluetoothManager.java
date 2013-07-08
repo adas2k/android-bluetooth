0e510c1deb1224ed23a5a1049b292a52ae83d7e4
adas2k <adas2k@gmail.com>
2013-07-07 20:24:35 -0400

    manager class +

--- library/src/com/thepine/android/bluetooth/BluetoothManager.java
+++ library/src/com/thepine/android/bluetooth/BluetoothManager.java
@@ -1,5 +1,24 @@
 package com.thepine.android.bluetooth;
 
+import android.content.Context;
+
 public class BluetoothManager{
+	
+	private static BluetoothManager mInstance = null;
+	
+	private Context mContext = null;
+	
+	private BluetoothManager(Context context) {
+		this.mContext = context;
+	}
+	
+	public synchronized BluetoothManager getInstance(Context context) {
+		if(mInstance == null) {
+			mInstance = new BluetoothManager(context);
+		}
+		return mInstance;
+	}
+	
+	
 	
 }
